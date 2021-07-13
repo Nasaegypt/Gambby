b6b4d2a1b12eeed73ad2f003d4a039ebde293c4e
NASA <nasaegypt@gmail.com>
2021-07-13 02:35:40 +0200

    link frontend with DB for services list & detailes

--- Gambby/urls.py
+++ Gambby/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('services/', include('service.urls')),
+    path('services/', include('service.urls', namespace='services')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- service/urls.py
+++ service/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'service'
 urlpatterns = [
     path('', views.service_list),
-    path('<int:id>', views.service_detail),
+    path('<int:id>', views.service_detail, name='service_detail'),
 
 ]
